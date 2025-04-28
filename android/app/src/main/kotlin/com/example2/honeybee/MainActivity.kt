9239a33627c01637a8ad327580f1361e2a122570
doit.flutterapps <doit.flutterapps@gmail.com>
2025-04-29 08:51:20 +0900

    07-2 [실습] 프로젝트 생성하고 추가 기능 설정하기 [실습] 동적 애니메이션을 활용한 인트로 페이지 만들기

---
+++ android/app/src/main/kotlin/com/example2/honeybee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example2.honeybee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/data/user.dart
@@ -0,0 +1,6 @@
+class HoneyBeeUser {
+  final String email;
+  final String? uid;
+  late String? hobby;
+  HoneyBeeUser({required this.email, required this.uid});
+}
