f391e2332bf1eb346c99cd4bdad090e6c60c1e2b
doit.flutterapps <doit.flutterapps@gmail.com>
2025-04-29 10:16:26 +0900

    07-3 [실습] 프로필 탭 만들기

---
+++ lib/view/main/sub/license_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class SNSLicensePage extends StatelessWidget {
+  const SNSLicensePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: LicensePage(
+        applicationName: 'HoneyBEE',
+        applicationVersion: '1.0.0',
+      ),
+    );
+  }
+}
